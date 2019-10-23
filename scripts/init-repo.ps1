8c94df38e1243b7f67e36b9e657e5b67e0c8fcb2
Jan Vilimek <jvilimek@users.noreply.github.com>
2019-10-23 09:15:54 +0200

    How to test locally section

--- scripts/init-repo.ps1
+++ scripts/init-repo.ps1
@@ -304,9 +304,49 @@ if ($existingCIBuildPipeline.Count -le 0)
     #no CI build definition exists = create!
 
 }
-$ciBuild = $existingCIBuildPipeline[0].id;
+$ciBuildId = $existingCIBuildPipeline[0].id;
 
 Write-Output '********* init branch policies *********'
+
+
+#see also https://www.visualstudiogeeks.com/azure/devops/azure-devops-cli-to-view-branch-policies
+Write-Output 'getting repository...'
+$repo = az repos list --org $azureDevOpsCollection --project $DevOpsProject --query "[?name=='$RepositoryName']" | ConvertFrom-Json;
+$idOfRepo = $repo.id
+#$existingPolicies = az repos policy list --org $azureDevOpsCollection --project $DevOpsProject --query "[?settings.scope[0].repositoryId=='$idOfRepo']"
+
+Write-Output 'getting existing repository policies...'
+$existingPolicies = az repos policy list --org $azureDevOpsCollection --project $DevOpsProject --query "[?settings.scope[0].repositoryId=='$($repo.id)' && settings.scope[0].refName=='refs/heads/develop']" | ConvertFrom-Json
+Write-Output 'cleaning up the policies...'
+$existingPolicies | select -ExpandProperty id |% { az repos policy delete --id $_ --org $azureDevOpsCollection --project $DevOpsProject --yes }
+
+
+Write-Output 'creating standard repository policies...'
+
+#load policy template
+$policies = Get-Content -Path C:\Users\JVilimek\Source\Repos\devops\scripts\branch-policies-develop.json | ConvertFrom-Json
+$tmpPolicyFile = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "tmp-policy.config");
+foreach($policy in $policies)
+{
+    if ($policy.settings.buildDefinitionId -eq -1)
+    {
+        #buildDefinitionId is defined in settings and set to default value => replace it with valid definition id
+        $policy.settings.buildDefinitionId = $ciBuildId
+    }
+    if ($policy.settings.scope.repositoryId -eq "00000000-0000-0000-0000-000000000000")
+    {
+        #repositoryId is defined in settings and set to default value => replace it with valid definition id
+        $policy.settings.scope.repositoryId = $idOfRepo
+    }
+    if (Test-Path -Path $tmpPolicyFile) {Remove-Item -Path $tmpPolicyFile -Force}
+    $policy | ConvertTo-Json -Depth 10 | Set-Content -Path $tmpPolicyFile -Encoding UTF8;
+    az repos policy create --org $azureDevOpsCollection --project $DevOpsProject --config $tmpPolicyFile;
+}
+#cleanup
+if (Test-Path -Path $tmpPolicyFile) {Remove-Item -Path $tmpPolicyFile -Force}
+
+#query for CLI see http://jmespath.org/specification.html
+#az repos policy list --org $azureDevOpsCollection --project $DevOpsProject --query "[?settings.scope[0].repositoryId=='$($repo.id)' && settings.scope[0].refName=='refs/heads/develop']"
 
 
 Write-Output ''
